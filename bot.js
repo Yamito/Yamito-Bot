78aaba039dba7efd19704bb3dca60b220c9f3b44
Yamito <51088408+Yamito@users.noreply.github.com>
2019-05-27 14:04:45 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,41 +1,29 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
 
+const client = new Discord.Client();
 
-bot.on("ready", () => {
-    console.log("Ready");
-    bot.user.setActivity('legit anything but fortnite', { type: 'Playing' });
-})
+ 
 
+client.on('ready', () => {
 
-bot.on("guildMemberAdd", (member) => {
-    const guild = member.guild;
-    const welcome = new Discord.RichEmbed()
-      .setTitle("Welcome to our server!")
-      .setColor([255, 92, 92])
-      .setFooter(`${moment().format('lll')}`)
-      .setThumbnail(member.user.avatarURL)
-  
-      bot.channels.filter(x => x.id == 522183451806400534)[0].sendEmbed(welcome);
+    console.log('I am ready!');
+
 });
 
+ 
 
-module.exports.run = async (bot, message, args) => {
-    let bicon = bot.user.displayAvatarURL;
-    let botembed = new Discord.RichEmbed()
-    .setDescription("Bot Information")
-    .setColor("#15f153")
-    .setThumbnail(bicon)
-    .addField("Bot Name", bot.user.username)
-    .addField("Created On", bot.user.createdAt);
+client.on('message', message => {
 
-    message.channel.send(botembed);
-}
+    if (message.content === 'ping') {
 
+       message.reply('pong');
 
+       }
 
+});
 
+ 
 
+// THIS  MUST  BE  THIS  WAY
 
-
-bot.login('process.env.BOT_TOKEN');
+client.login(process.env.BOT_TOKEN);
